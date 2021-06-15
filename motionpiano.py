4906b8d509e3885d9618616a313372a6469fc361
arpruss <arpruss@gmail.com>
2021-06-15 15:39:15 -0500

    compat

--- motionpiano.py
+++ motionpiano.py
@@ -36,17 +36,17 @@ while True:
     if comparisonFrame is None:
         aspect = frame.shape[1] / frame.shape[0]
         height = math.floor(WIDTH / aspect)
-        keys = np.zeros((height,WIDTH),np.uint8)
-        blankOverlay = np.zeros((height,WIDTH,3),np.uint8)
+        keys = np.zeros((height,WIDTH),dtype=np.uint8)
+        blankOverlay = np.zeros((height,WIDTH,3),dtype=np.uint8)
         rects = []
 
         for i in range(numKeys):
-            x0 = math.floor(WIDTH*i/numKeys)
-            x1 = math.floor(WIDTH*(i+1)/numKeys)-1
+            x0 = WIDTH*i//numKeys
+            x1 = WIDTH*(i+1)//numKeys-1
 
-            r = [[x0,0],[x1,math.floor(NOTE_HEIGHT*height)]]
+            r = [(x0,0),(x1,int(NOTE_HEIGHT*height))]
             rects.append(r)
-            cv2.rectangle(keys, r[0], r[1], color=1+i, thickness=cv2.FILLED)
+            cv2.rectangle(keys, r[0], r[1], int(i+1), cv2.FILLED)
 
     frame = cv2.flip(cv2.resize(frame, (WIDTH,height)), 1)
     blurred = cv2.GaussianBlur(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), (21, 21), 0)
@@ -81,7 +81,7 @@ while True:
     overlay = blankOverlay.copy()
     for i in range(numKeys):
         if 1+i+COMPARISON_VALUE in sum:
-            cv2.rectangle(overlay, rects[i][0], rects[i][1], color=(255,255,255), thickness=cv2.FILLED)
+            cv2.rectangle(overlay, rects[i][0], rects[i][1], (255,255,255), cv2.FILLED)
             if not playing[i]:
                 player.note_on(NOTES[i],127)
                 playing[i] = True
@@ -89,7 +89,7 @@ while True:
             if playing[i]:
                 player.note_off(NOTES[i],127)
                 playing[i] = False
-        cv2.rectangle(overlay, rects[i][0], rects[i][1], color=(0,255,0))
+        cv2.rectangle(overlay, rects[i][0], rects[i][1], (0,255,0), 1)
             
     cv2.imshow("MotionPiano", cv2.addWeighted(frame, 1, overlay, 0.25, 1.0))
     if (cv2.waitKey(1) & 0xFF) == 27:
